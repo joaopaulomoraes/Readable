a8637dd24ccff91d8c856d1355baf235c168f7a1
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-02-28 09:42:01 -0300

    feat: Categories action

---
+++ client/src/api/categories/index.js
@@ -0,0 +1,18 @@
+import ReadableAPI from '../'
+
+/**
+ * @description Get all categories of api
+ * @returns {object} All Categories and the action type
+ */
+const getCategories = () => {
+  return ReadableAPI.get('/categories')
+}
+
+/**
+ * @description Get all posts based on a category
+ * @param {string} category -
+ * @returns {object} The post data filtered by a category and the type of action and type of action
+ */
+export const getCategoriesByPost = category => {
+  return ReadableAPI.get(`/${category}/posts`)
+}
