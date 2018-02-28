d960f36cb2d76f9d841b70993e3795a47c92effd
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-02-28 11:19:51 -0300

    feat: Added support for routes to serve redux-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import {
+  Route,
+  Switch
+} from 'react-router-dom'
 import Reboot from 'material-ui/Reboot'
 import MenuBar from './MenuBar'
 import Categories from './categories'
+import routes from '../routes'
+import { connect } from 'react-redux'
+
+export const RouteWithSubRoutes = route => (
+  <Route
+    path={route.path}
+    render={props => (
+      <route.component
+        exact={route.exact}
+        routes={route.routes}
+        {...props}
+      />
+    )}
+  />
+)
 
 const App = props => {
   return (
@@ -9,8 +28,21 @@ const App = props => {
       <Reboot />
       <MenuBar />
       <Categories />
+
+      <ConnectedSwitch>
+        {routes.map((route, i) => 
+          <RouteWithSubRoutes
+            key={i}
+            {...route}
+          />
+        )}
+      </ConnectedSwitch>
     </div>
   )
 }
+
+const ConnectedSwitch = connect(state => ({
+  location: state.location
+}))(Switch)
 
 export default App
