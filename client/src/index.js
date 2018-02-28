72bcaa8182befa6d924acad31f20a074b5e5d248
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-02-28 02:09:16 -0300

    feat: Provider to store and connecting routes with redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+import { ConnectedRouter } from 'react-router-redux'
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
