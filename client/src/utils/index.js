4bf9ea7d2e357e773d3bf7330f0fa515da8fa5db
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-02-28 02:30:09 -0300

    refactor: Export of methods

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -5,7 +5,7 @@ import moment from 'moment'
  * @description Generates a current timestamp (now) in unix format
  * @returns {number} Current timestamp in unix format
  */
-const unixTimestamp = () => {
+export const unixTimestamp = () => {
   return +moment()
 }
 
@@ -13,6 +13,6 @@ const unixTimestamp = () => {
  * @description Generate an id for posts, comments and everything else
  * @returns {string} uuid
  */
-const generateId = () => {
+export const generateId = () => {
   return uuid()
 }
